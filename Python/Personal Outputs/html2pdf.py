ed671e90dfdf8a4b25e047d332784a59e113061f
kookoowaa <30887470+kookoowaa@users.noreply.github.com>
2017-09-12 16:21:08 +0900

    170912 update

--- Python/Personal Outputs/html2pdf.py
+++ Python/Personal Outputs/html2pdf.py
@@ -8,9 +8,9 @@ Vertical_option = {
     'orientation': 'Landscape'}
 # options @ https://wkhtmltopdf.org/usage/wkhtmltopdf.txt
 
-base_url = "http://ranking.uos.ac.kr/material/bigdata/week_4/web_scraping.html#"
+base_url = "http://ranking.uos.ac.kr/material/bigdata/week_5/web_mining.html#"
 url_list= []
-for i in range(1,33):
+for i in range(1,56):
     url_list.append(base_url+str(i))
 
-pdfkit.from_url(url_list, "d:/Web Scraping.pdf", configuration=config, options = Vertical_option)
+pdfkit.from_url(url_list, "d:/Web mining.pdf", configuration=config, options = Vertical_option)
