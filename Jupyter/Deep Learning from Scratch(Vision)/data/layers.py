791abf9a7ef8f23944e1ef7e55ddbee0a77fc453
Park, Pablo Chanwoo <30887470+kookoowaa@users.noreply.github.com>
2018-06-30 01:06:34 +0900

    DL_ implementing packpropagation

--- Jupyter/Deep Learning from Scratch(Vision)/data/layers.py
+++ Jupyter/Deep Learning from Scratch(Vision)/data/layers.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import numpy as np
-from common.functions import *
-from common.util import im2col, col2im
+from data.functions import *
+from data.util import im2col, col2im
 
 
 class Relu:
